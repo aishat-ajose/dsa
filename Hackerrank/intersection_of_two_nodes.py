c9a6bedf2fcffc30f085d5ee245d0c94d157d670
Ajose Aishat <aishatajoseh@yahoo.com>
2021-12-30 16:12:58 +0100

    Solved hackerrank linkedlist interview prep questions

--- Hackerrank/intersection_of_two_nodes.py
+++
@@ -1,9 +0,0 @@
-def findMergeNode(head1, head2):
-    a = head1
-    b = head2
-    
-    while(a != b):
-        a = a.next if a else head2
-        b = b.next if b else head1
-        
-    return a.data
